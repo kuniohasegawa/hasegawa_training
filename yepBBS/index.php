74520852defacd8c71a429e1e5adea41873356d2
kuniohasegawa <hasegawa@ye-p.co.jp>
2018-07-02 15:22:17 +0900

    html

---
+++ yepBBS/index.php
@@ -0,0 +1,17 @@
+<DOCTYPE html>
+<html lang="ja">
+<head>
+	<meta charset="utf-8">
+	
+	<link rel="stylesheet" href="css/style.css">
+	<title>yepBBS</title>
+</head>
+
+<body>
+	<div class="header">
+		<h1><span>yep</span>BBS</h1>
+	</div>
+</body>
+
+</html>
+
