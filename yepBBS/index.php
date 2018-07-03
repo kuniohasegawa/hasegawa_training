06ccc644dc547d209d4b4daf256a6f909908fc51
kuniohasegawa <hasegawa@ye-p.co.jp>
2018-07-03 10:17:49 +0900

    修正

--- yepBBS/index.php
+++ yepBBS/index.php
@@ -1,9 +1,9 @@
-<DOCTYPE html>
+<!DOCTYPE html>
 <html lang="ja">
 <head>
-	<meta charset="utf-8">
+	<meta charset="UTF-8">
 	
-	<link rel="stylesheet" href="css/style.css">
+	<link rel="stylesheet" href="style.css">
 	<title>yepBBS</title>
 </head>
 
@@ -11,7 +11,26 @@
 	<div class="header">
 		<h1><span>yep</span>BBS</h1>
 	</div>
+	<div class="time">
+		<?php echo date("y/m/d"); ?>
+	</div>
+
+	
+	
+	<div class="footer">
+		
+	<p>ルーム登録	</p>
+		<div class="room-name">ルーム名
+		<input class="room" type="text" name="room"><br>
+		<span class="insert">必ず入力してください</span><br>
+		</div>
+		<div class="name">名前
+		<input class=""type="text" name="name"><br>
+		<input type="submit" value="登録" ><br>
+		</div>
+	</div>	
+	
+
 </body>
 
 </html>
-
