63ffee5960e222bb9cd398c7d8138b04a9de14e7
kuniohasegawa <hasegawa@ye-p.co.jp>
2018-07-05 17:26:31 +0900

    dbへのインサート可能に

--- yepBBS/index.php
+++ yepBBS/index.php
@@ -2,36 +2,26 @@
 session_start();
 require('dbconnect.php');
 
-//エラー時のメッセージ
-//$error_msg='必ず入力してください';
-
-//エラーの確認
 if(!empty($_POST)){
-	if(isset($_POST['room']) && $_POST['room']==''){
-		$error['room'] = 'blank';
+	if(isset($_POST['title']) && $_POST['title']==''){
+		$error['title'] = 'blank';
 	}
-	if(isset($_POST['name']) && $_POST['name']==''){
-		$error['name'] = 'blank';
-	}
-
-	if(empty($error)){
-		$_SESSION['index'] = $_POST;
-//		header('Location:room.php ');
-//		exit();
+	if(isset($_POST['user_name']) && $_POST['user_name']==''){
+		$error['user_name'] = 'blank';
 	}
 }	
 
 
+//var_dump($SESSION);
+//echo var_dump($SESSION['index']['room'];
 //dbへデータを挿入
 if(!empty($_POST)){
-	$member=$db->prepare('INSERT INTO rooms SET title=?, user_name=?, modified=getlastmod(), created=NOW()');
+	$member=$db->prepare('INSERT INTO rooms SET title=?, user_name=?, modified=NOW(), created=NOW()');
 	$member->execute(array(
-		$_SESSION['index']['room'],
-		$_SESSION['index']['name'],
+	 	$_POST['title'],
+		$_POST['user_name']
 		));
-	unset($_SESSION['index']);
-
-	exit();
+var_dump($_POST);	
 }
 
 date_default_timezone_set('Asia/Tokyo');
@@ -61,16 +51,16 @@ date_default_timezone_set('Asia/Tokyo');
 
 	<form method="post" action="" >		
 		<p class="regi">ルーム登録</p>
-		<span class="roomname">ルーム名</span><input class="room" type="text" name="room"><br>
-	<span class="error_msg"><?php if(isset($error['room']) == 'blank'): ?>
+		<span class="roomname">ルーム名</span><input class="room" type="text" name="title"><br>
+	<span class="error_msg"><?php if(isset($error['title']) == 'blank'): ?>
 		<?php echo '必ず入力してください'.'<br>';?>
 		<?php endif; ?>
 	</span>
-		<span class="list-name">名前</span><input class="name"type="text" name="name">
+		<span class="list-name">名前</span><input class="name"type="text" name="user_name">
 	
 		<input class="btn" type="submit" value="登録" ><br>
 	<span class="error_msg">
-		<?php if(isset($error['name']) == 'blank'): ?>
+		<?php if(isset($error['user_name']) == 'blank'): ?>
 		<?php echo '必ず入力してください'.'<br>'; ?>
 		<?php endif; ?>
 	</span>
