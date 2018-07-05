0aee238e1aa7de52078dab37fce76489a7fbf33c
kuniohasegawa <hasegawa@ye-p.co.jp>
2018-07-05 17:24:15 +0900

    dbインサート成功

--- yepBBS/dbconnect.php
+++ yepBBS/dbconnect.php
@@ -1,6 +1,6 @@
 <?php
 try{
-	$db=new PDO('mysql:dbname=yepbbs; host=192.168.2.59 ;charset=utf8','root','P@ssw0rd');
+	$db=new PDO('mysql:dbname=yepbbs; host=127.0.0.1 ;charset=utf8','root','P@ssw0rd');
 }catch(PDOException $e){
 	echo 'DB接続エラー: '.$e->getMessage();
 }
