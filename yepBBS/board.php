51defd3fa054cb93e8e64286ce504fd273cb1135
kuniohasegawa <hasegawa@ye-p.co.jp>
2018-07-24 17:54:16 +0900

    インデントの修正、不要な関数の削除

--- yepBBS/board.php
+++ yepBBS/board.php
@@ -1,92 +1,98 @@
 <?php
-session_start();
-require('dbconnect.php');
-$result=preg_match("/^[0-9]+$/",$_POST['res']);
+require_once('dbconnect.php');
+require_once('structured.php');
+$room_id = intval($_GET['id']);
 
-if(!empty($_POST)){
-
-	if(!empty($_POST['res']) && $result == 0){
+if (!empty($_POST)) {
+	if (!empty($_POST['res'])) {
+		$result=preg_match("/^[0-9]+$/",$_POST['res']);
+	}
+	if ((!empty($_POST['res'])) && ($result == 0)) {
 		$error['thread_number'] = 'error1';
 	}
-	if(isset($_POST['text']) && $_POST['text']==''){
+	if ((isset($_POST['text'])) && ($_POST['text']=='')) {
 		$error['text'] = 'blank';
 	}
-	if(isset($_POST['user_name']) && $_POST['user_name']==''){
+	if ((isset($_POST['user_name'])) && ($_POST['user_name']=='')) {
 		$error['user_name'] = 'blank';
 	}
 }
 
-$room_id=$_GET['id'];
-//代替案
-$num=$db->prepare('select text from comments where room_id = ?');
-$num->execute(array($_GET['id']));
-$numbers=$num->fetchAll();
+$num = $db->prepare('select text from comments where room_id = ?');
+$num->execute(array($room_id));
+$numbers = $num->fetchAll();
 $numbers = count($numbers)+1;
+$numbers = intval($numbers);
+if ($numbers == 0){
+	$numbers = null;
+}
 
-$get_id=intval($_GET['id']);
+$_POST['res'] = intval($_POST['res']);
+	if($_POST['res'] == 0) {
+		$_POST['res'] = null;
+	}
+
 
 //threadnumber照合用のバリデーション
-$check=$db->prepare('select * from comments where number = ? and room_id = ? ');
+$check = $db->prepare('select * from comments where number = ? and room_id = ? ');
 $check->execute(array(
 	$_POST['res'],
-	$_GET['id']
+	$room_id
 ));
-$check_thread_number=$check->fetch();
+$check_thread_number = $check->fetch();
 
 //返信の番号がtextの値よりも小さいか
 $text = $db->prepare('select count(text) as count from comments where room_id = ?');
 $text->execute(array(
-	$_GET['id']));
+	$room_id));
 $total_text = $text->fetch();
 
 //返信先の値がnumberよりも大きくなってしまった場合にerror2を
-if($total_text['count'] < $_POST['res']){
+if ($total_text['count'] < $_POST['res']) {
 	$error['res'] = 'error2';
 }
 
 //  thread_numberあり、なしでinsertをわける
-if($total_text['count'] <= 100){
-	if(empty($error)){
-		if(empty($_POST['res']) && ($total_text['count'] > $_POST['res'])){
+if ($total_text['count'] <= 100) {
+	if (empty($error)) {
+		if (empty($_POST['res']) && ($total_text['count'] > $_POST['res'])) {
 			$bbs =$db->prepare('INSERT INTO comments SET room_id=? ,number=? , text=?, user_name=?,modified=NOW(),created=NOW()');
-			$test=$bbs->execute(array(
-				$_GET['id'],
-				intval($numbers),
-				$_POST['text'],
-				$_POST['user_name']
-			));
-		}elseif(!empty($_POST['res']) && is_null($check_thread_number['thread_number'])){
-			$bbs =$db->prepare('INSERT INTO comments SET room_id=? ,thread_number=? ,number=? , text=?, user_name=?,modified=NOW(),created=NOW()');
-			$test=$bbs->execute(array(
-				$_GET['id'],
-				intval($_POST['res']),
-				intval($numbers),
-				$_POST['text'],
-				$_POST['user_name']
-						));
-		 }else{
+				$test = $bbs->execute(array(
+					$room_id,
+					$numbers,
+					$_POST['text'],
+					$_POST['user_name']
+				));
+		} elseif ((!empty($_POST['res'])) && (is_null($check_thread_number['thread_number']))) {
+			$bbs = $db->prepare('INSERT INTO comments SET room_id=? ,thread_number=? ,number=? , text=?, user_name=?,modified=NOW(),created=NOW()');
+				$test = $bbs->execute(array(
+					$room_id,
+					$_POST['res'],
+					$numbers,
+					$_POST['text'],
+					$_POST['user_name']
+							));
+		 } else {
 	  $error['res'] = 'error2';
 		 }}
-}else{
+} else {
 		$error['max'] = 'error3';
 }
 
+$relation = $db->prepare('SELECT * FROM comments WHERE room_id=? and thread_number IS NULL ORDER BY created ASC');
+$relation->execute(array($room_id));
+$nullthreads = $relation->fetchAll();
 
-//thread_numberがないものとあるものを分けた
-	$relation=$db->prepare('SELECT * FROM comments WHERE room_id=? and thread_number IS NULL ORDER BY created ASC');
-	$relation->execute(array($get_id));
-	$nullthreads=$relation->fetchAll();
-
-	$stanby=$db->prepare('SELECT * FROM comments WHERE room_id=?  ORDER BY created ASC');
-	$stanby->execute(array(
-		$get_id,
-		));
-	$threads=$stanby->fetchAll();
+$stanby = $db->prepare('SELECT * FROM comments WHERE room_id=?  ORDER BY created ASC');
+$stanby->execute(array(
+	$room_id,
+	));
+   $threads = $stanby->fetchAll();
 
 
 //関数
 function h($value){
-	echo htmlspecialchars($value, ENT_QUOTES, UTF-8);
+	echo htmlspecialchars($value, ENT_QUOTES, 'UTF-8');
 }
 date_default_timezone_set('Asia/Tokyo');
 ?>
@@ -95,7 +101,6 @@ date_default_timezone_set('Asia/Tokyo');
 <html lang="ja">
 <head>
 	<meta charset="UTF-8">
-
 	<link rel="stylesheet" href="style.css">
 	<title>yepBBS</title>
 </head>
@@ -113,23 +118,26 @@ date_default_timezone_set('Asia/Tokyo');
 <div class="main">
 	<div class="text">
 		<?php foreach($nullthreads as $nullthread):?>
-		<section class="heads">
-			<div class="text_number"><?php h($nullthread['number']);?></div>
-			<div class="text_name"><?php h($nullthread['user_name']); ?></div>
-		</section>
-			<div class="text_date"><?php h(date('Y/m/d/h/i',strtotime($nullthread['created'])));?></div>
-			<div class="text_content"><?php h($nullthread['text']); ?><br><br></div>
+			<div class="style_heads">
+				<div class="heads">
+					<div class="text_number"><?php h($nullthread['number']);?></div>
+					<div class="text_name"><?php h($nullthread['user_name']); ?></div>
+					<div class="text_date"><?php h(date('Y/m/d/h/i',strtotime($nullthread['created'])));?></div>
+					<div class="text_content"><?php h($nullthread['text']); ?><br><br></div>
+				</div>
+			<div>
 	</div>
-
 			<div class="thread_text">
 				<?php foreach($threads as $thread):?>
 					<?php if($thread['thread_number'] == $nullthread['number']): ?>
-					<section class="threads">
-						<div class="res_number"><?php h($thread['number']) ;?></div>
-						<div class="res_name"><?php h($thread['user_name']); ?></div>
-						<div class="res_date"><?php h(date('Y/m/d/h/i',strtotime($thread['created'])));?></div>
-					</section>
-						<div class="res_content"><?php h($thread['text']); ?><br><br></div>
+						<div class = "style_threads">
+							<div class="threads">
+								<div class="res_number"><?php h($thread['number']) ;?></div>
+								<div class="res_name"><?php h($thread['user_name']); ?></div>
+								<div class="res_date"><?php h(date('Y/m/d/h/i',strtotime($thread['created'])));?></div>
+							</div>
+								<div class="res_content"><?php h($thread['text']); ?><br><br></div>
+						</div>
 					<?php endif; ?>
 				<?php endforeach; ?>
 			</div>
@@ -137,13 +145,12 @@ date_default_timezone_set('Asia/Tokyo');
 </div>
 <!--フォーム入力-->
 	<div class="footer">
-
 		<form method="post" action="" >
 			<ul>
 				<li class="title_register"><p class="regi">コメント登録</p></li>
 				<li>
-					<label class="list_title">返信</label><input class="res" type="text" name="res"><br>
-					<label class="error_msg">
+					<label class="list_title">返信<input class="res" type="text" name="res"></label><br>
+					<div class="error_msg">
 						<?php if(isset($error['thread_number']) && $error['thread_number'] == 'error1'):?>
 							<?php echo '半角以外の数字が入力されています' ;?>
 						<?php elseif(isset($error['res']) && $error['res'] == 'error1'): ?>
@@ -151,33 +158,34 @@ date_default_timezone_set('Asia/Tokyo');
 						<?php elseif($error['res'] == 'error2'): ?>
 							<?php echo '返信先が間違っています'; ?>
 						<?php elseif(isset($error['max']) && $error['max'] == 'error3'):?>
-							<?php  echo 'コメントが100件になりました。新しいRoomを作成してください'; ?>
+							<?php  echo 'コメントが100件になりました。新しいRoomを作成してください'; ?><br>
 						<?php endif; ?>
-					</label>
+					</div>
 				</li>
 				<li>
-						<label class="list_title">コメント</label><textarea class="text" name="text" raws="30" cols="35" ></textarea><br>
-					<label class="error_msg">
-						<?php if(isset($error['text']) == 'blank'): ?>
-						<?php echo '必ず入力してください'.'<br>'; ?>
-						<?php endif; ?>
-					</label>
+					<label class="list_title">コメント</label><textarea class="text" name="text" raws="30" cols="35" ></textarea><br>
+						<div class="error_msg">
+							<?php if(isset($error['text']) == 'blank'): ?>
+								<p><?php echo '必ず入力してください'; ?></p><br>
+							<?php endif; ?>
+						</div>
 				</li>
 				<li>
-					<label class="list_title">名前</label><input class="name" type="text" name="user_name"><br>
-					<label class="error_msg">
-							<?php if(isset($error['user_name']) == 'blank'): ?>
-						<?php echo '必ず入力してください' ; ?>
-						<?php endif; ?>
-					</label>
+					<div class="name_list">
+						<label class="list_title">名前</label><input class="name" type="text" name="user_name">
+						<input class="btn" type="submit" value="登録" >
+					</div>
 				</li>
 				<li>
-					<input class="btn" type="submit" value="登録" ><br>
+					<div class="error_msg">
+						<?php if(isset($error['user_name']) == 'blank'): ?>
+							<p><?php echo '必ず入力してください' ; ?></p>
+						<?php endif; ?>
+					</div>
 				</li>
 			</ul>
 		</form>
 	</div>
 
 </body>
-
 </html>
--- yepBBS/index.php
+++ yepBBS/index.php
@@ -1,36 +1,34 @@
 <?php
-session_start();
-require('dbconnect.php');
+require_once('dbconnect.php');
 
-if(!empty($_POST)){
-	if(isset($_POST['title']) && $_POST['title']==''){
+if (!empty($_POST)) {
+	if (isset($_POST['title']) && ($_POST['title'] == '')){
 		$error['title'] = 'blank';
 	}
-	if(isset($_POST['user_name']) && $_POST['user_name']==''){
+	if (isset($_POST['user_name']) && ($_POST['user_name'] == '')){
 		$error['user_name'] = 'blank';
 	}
-}
 
-//dbへデータを挿入
-if(isset($_POST) && empty($error)){
+
+if (isset($_POST) && (empty($error))) {
 	$member = $db->prepare('INSERT INTO rooms SET title=?, user_name=?, modified=NOW(), created=NOW()');
 	$member->execute(array(
-	 	$_POST['title'],
+		$_POST['title'],
 		$_POST['user_name']
 	));
-}
+}}
 
+$bord_p = $db->query('SELECT rooms.*, comments.modified AS comments_modified  FROM rooms JOIN comments ORDER BY modified DESC');
+$boards = $bord_p->fetchAll();
+//var_dump(max($boards['comments_modified']));
 
-
-////dbから情報を取得
-$bord_p = $db->query('SELECT id,title, user_name, modified FROM rooms ORDER BY modified DESC');
-//$room_id = $db->query('SELCT room_id FROM rooms JOIN comments ON rooms.id = comments.rooms_id');
-		$boards=$bord_p->fetchAll();
-
-
-//関数
-function h($value){
-	echo htmlspecialchars($value, ENT_QUOTES, UTF-8);
+//$modified = $db->prepare('SELECT max(modified) AS modified FROM comments WHERE room_id = ?');
+//	$modified->execute(array(
+//			$boards['id']
+//		));
+//$last_comment_modified = $modified->fetchAll();
+function h($value) {
+	echo htmlspecialchars($value, ENT_QUOTES, 'UTF-8');
 }
 
 //ローカルタイムの設定
@@ -41,17 +39,15 @@ date_default_timezone_set('Asia/Tokyo');
 <html lang="ja">
 <head>
 	<meta charset="UTF-8">
-<!--	<meta name="viewport" content="width=device-width,initial-scale=1">  -->
 	<link rel="stylesheet" href="style.css">
 	<title>yepBBS</title>
-	<script language="javascript">
-	resizeTo(850,550);
-	</script>
+<script language = "javascript">
+resizeTo(850,550);
+</script>
 </head>
 
 <body>
 <div class="display-width">
-
 	<div class="header">
 		<h1><span>yep</span>BBS</h1>
 	</div>
@@ -60,9 +56,7 @@ date_default_timezone_set('Asia/Tokyo');
 		<?php echo date("h/i"); ?>
 
 	</div>
-
 	<div class="main-contents">
-	<!-- テーブルの作成 -->
 	<table class="contents-table" align="center">
 			<tr>
 				<th class="title-detail">タイトル</th>
@@ -71,15 +65,13 @@ date_default_timezone_set('Asia/Tokyo');
 				<th>最終更新日</th>
 				</div>
 			</tr>
-	<!-- データベースの情報を表示 -->
-		<?php foreach($boards as $board):?>
-		<tr>
-			<td class="detail"><a href="board.php?id=<?php echo $board['id']?>"><?php h($board['title']); ?></a></td>
-			<td class="detail"><?php h($board['user_name']); ?></td>
-			<td class="detail"><?php h($board['modified']); ?></td>
-		</tr>
-		<?php endforeach; ?>
-
+		<?php foreach($boards as $board) : ?>
+			<tr>
+			<td class="detail"><a href="board.php?id = <?php echo $board['id']?>;"><?php h($board['title']); ?></a></td>
+				<td class="detail"><?php h($board['user_name']); ?></td>
+				<td class="detail"><?php h($board['comments_modified']); ?></td>
+			</tr>
+		<?php endforeach ; ?>
 	</table>
 	</div>
 
@@ -87,21 +79,21 @@ date_default_timezone_set('Asia/Tokyo');
 	<div class="footer">
 		<form method="post" action="" >
 			<p class="regi">ルーム登録</p>
-			<span class="roomname">ルーム名</span><input class="room" type="text" name="title"><br>
-		<span class="error_msg"><?php if(isset($error['title']) == 'blank'): ?>
-			<?php echo '必ず入力してください'.'<br>';?>
-			<?php endif; ?>
-		</span>
+			<span class="roomname">ルーム名</span><input class = "room" type = "text" name = "title"><br>
+			<span class="error_msg">
+				<?php if(isset($error['title']) == 'blank'): ?>
+					<?php echo '必ず入力してください'.'<br>';?>
+				<?php endif; ?>
+			</span>
 			<span class="list-name">名前</span><input class="name"type="text" name="user_name">
 			<input class="btn" type="submit" value="登録" ><br>
-		<span class="error_msg">
-			<?php if(isset($error['user_name']) == 'blank'): ?>
-			<?php echo '必ず入力してください'.'<br>'; ?>
-			<?php endif; ?>
-		</span>
+			<span class="error_msg">
+				<?php if (isset($error['user_name']) == 'blank'): ?>
+					<?php echo '必ず入力してください'.'<br>'; ?>
+				<?php endif; ?>
+			</span>
 		</form>
 	</div>
 </div>
 </body>
-
 </html>
