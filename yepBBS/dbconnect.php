2173d8bf89227213975091536f74e265e6c62133
kuniohasegawa <hasegawa@ye-p.co.jp>
2018-07-24 11:34:28 +0900

    修正

--- yepBBS/dbconnect.php
+++ yepBBS/dbconnect.php
@@ -1,7 +1,7 @@
 <?php
 try{
 	$db = new PDO('mysql:dbname=yepbbs; host=127.0.0.1; charset=utf8', 'root', 'P@ssw0rd');
-} catch (PDOExcep tion $e) {
+} catch (PDOException $e) {
 	echo 'DB接続エラー: '.$e->getMessage();
 }
 
